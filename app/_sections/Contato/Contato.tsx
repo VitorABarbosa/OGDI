30cbae6878f0539c2e4269c1454ea8927920fad8
Vitor Barbosa <vitor.barbosa232006@gmail.com>
2026-06-05 13:59:01 -0300

    Tune contact watermark contrast

--- app/_sections/Contato/Contato.tsx
+++ app/_sections/Contato/Contato.tsx
@@ -11,7 +11,7 @@ export function Contato() {
         width={1024}
         height={1536}
         aria-hidden
-        className="pointer-events-none absolute right-[-5vw] top-1/2 z-0 h-[78%] w-auto max-w-none -translate-y-1/2 select-none object-contain opacity-[.18] [filter:brightness(0)_invert(1)] max-lg:right-[-22vw] max-lg:opacity-[.12] max-md:hidden"
+        className="pointer-events-none absolute right-[-5vw] top-1/2 z-0 h-[78%] w-auto max-w-none -translate-y-1/2 select-none object-contain opacity-[.34] [filter:grayscale(1)_invert(.72)_brightness(1.08)_contrast(.95)_drop-shadow(0_0_18px_rgba(255,255,255,.18))] max-lg:right-[-22vw] max-lg:opacity-[.22] max-md:hidden"
         sizes="(min-width: 1024px) 42vw, 0px"
       />
       <div className="wrap relative z-[1]">
