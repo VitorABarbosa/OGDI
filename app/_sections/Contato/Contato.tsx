4d6337b44436d3658eca52576bf574895433c5f0
Vitor Barbosa <vitor.barbosa232006@gmail.com>
2026-06-05 13:52:29 -0300

    Refine contact watermark visibility

--- app/_sections/Contato/Contato.tsx
+++ app/_sections/Contato/Contato.tsx
@@ -8,11 +8,11 @@ export function Contato() {
       <Image
         src="/Fachada.png"
         alt=""
-        width={1120}
-        height={3744}
+        width={1024}
+        height={1536}
         aria-hidden
-        className="pointer-events-none absolute right-0 top-1/2 z-0 h-1/2 w-auto max-w-none -translate-y-1/2 select-none object-contain opacity-[.12] grayscale contrast-125 max-lg:right-[-18vw] max-lg:opacity-[.08] max-md:hidden"
-        sizes="(min-width: 1024px) 20vw, 0px"
+        className="pointer-events-none absolute right-[-5vw] top-1/2 z-0 h-[78%] w-auto max-w-none -translate-y-1/2 select-none object-contain opacity-[.18] [filter:brightness(0)_invert(1)] max-lg:right-[-22vw] max-lg:opacity-[.12] max-md:hidden"
+        sizes="(min-width: 1024px) 42vw, 0px"
       />
       <div className="wrap relative z-[1]">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-[clamp(48px,6vw,100px)] items-start">
