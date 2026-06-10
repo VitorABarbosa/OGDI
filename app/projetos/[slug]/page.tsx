345a6701f00308726d788455a0d21323b855026a
Vitor Barbosa <vitor.barbosa232006@gmail.com>
2026-06-10 10:55:54 -0300

    feat(empreendimento): prototype ripple background

--- app/projetos/[slug]/page.tsx
+++ app/projetos/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { EmpInfo } from "@/app/_sections/EmpreendimentoPage/EmpInfo";
 import { EmpGaleria } from "@/app/_sections/EmpreendimentoPage/EmpGaleria";
 import { EmpAtuacao } from "@/app/_sections/EmpreendimentoPage/EmpAtuacao";
 import { EmpProximos } from "@/app/_sections/EmpreendimentoPage/EmpProximos";
+import { EmpRippleBackground } from "@/app/_sections/EmpreendimentoPage/EmpRippleBackground";
 import { CtaBand } from "@/components/ui/CtaBand";
 import { RevealController } from "@/components/Reveal/RevealController";
 
@@ -41,10 +42,15 @@ export default async function Page({
     <main>
       <RevealController />
       <EmpHero p={p} />
-      <EmpInfo p={p} />
-      <EmpGaleria p={p} />
-      <EmpAtuacao />
-      <EmpProximos others={others} />
+      <div className="relative overflow-hidden bg-white">
+        <EmpRippleBackground />
+        <div className="relative z-[1]">
+          <EmpInfo p={p} />
+          <EmpGaleria p={p} />
+          <EmpAtuacao />
+          <EmpProximos others={others} />
+        </div>
+      </div>
       <CtaBand
         title={`Interesse no ${p.name}?`}
         text="Fale com a equipe da Open Group e receba mais informações sobre o empreendimento e a operação."
