eec29026590450f3a8054b7bf18b6fb4830986b6
Vitor Barbosa <vitor.barbosa232006@gmail.com>
2026-06-13 10:46:11 -0300

    refactor(i18n): move rotas para app/[locale] e ajusta imports

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,11 @@
-import { Hero } from "./_sections/Hero/Hero";
-import { Projetos } from "./_sections/Projetos/Projetos";
-import { Atuacao } from "./_sections/Atuacao/Atuacao";
-import { Modelos } from "./_sections/Modelos/Modelos";
-import { Manifesto } from "./_sections/Manifesto/Manifesto";
-import { Clientes } from "./_sections/Clientes/Clientes";
-import { Contato } from "./_sections/Contato/Contato";
-import { PageSectionRail } from "./_sections/PageSectionRail";
+import { Hero } from "@/app/_sections/Hero/Hero";
+import { Projetos } from "@/app/_sections/Projetos/Projetos";
+import { Atuacao } from "@/app/_sections/Atuacao/Atuacao";
+import { Modelos } from "@/app/_sections/Modelos/Modelos";
+import { Manifesto } from "@/app/_sections/Manifesto/Manifesto";
+import { Clientes } from "@/app/_sections/Clientes/Clientes";
+import { Contato } from "@/app/_sections/Contato/Contato";
+import { PageSectionRail } from "@/app/_sections/PageSectionRail";
 import { IntroVideo } from "@/components/Intro/IntroVideo";
 import { RevealController } from "@/components/Reveal/RevealController";
 
